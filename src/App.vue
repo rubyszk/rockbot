ca71807cfef0b2cc87bcc7ded08377536655b0ef
ru <ruby@vuniverse.com>
2021-04-22 20:04:40 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,73 @@
 <template>
   <div id="app">
+    <h1>Rockbot Lite</h1>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//header
+//footer - now playing
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  
+  // components: {
+  //   HelloWorld
+  // }
+  data() {
+    return {
+      // isLoading: false,
+      nowPlaying: {},
+      upNext: {},
+      browseArtists: {},
+      trendingArtists: {},
+      searchResults: {}
+    }
+  },
+  computed: {
+    window: () => window,
+    isPlaying() {
+      return `${this.nowPlaying}`
+    },
+  },
+  methods: {
+    fetchNowPlaying(){ 
+      // API Call also fetches Queue
+      // fetch from url
+      // .then((res) => res.json())
+      //.then((data)=> {
+        //this.nowPlaying = data.res.now_playing
+      // })
+      // fetch every 30 seconds
+      // window.setInterval(() => {
+        // this.nowPlaying()
+      // }, 30000)
+    },
+    sayHello(){
+      window.setInterval(() => {
+        console.log('hellooooo')
+      }, 1000)
+    },
+    fetchBrowse(){
+      // API Call
+
+    },
+    fetchTopArtists(){
+      // API Call
+
+    },
+    mounted() {
+      // Call on mount to fetch data
+      // (useEffect/componentDidMount)
+
+      this.fetchNowPlaying();
+      this.fetchBrowse();
+      this.fetchTopArtists();
+    }
+
+
   }
 }
 </script>
